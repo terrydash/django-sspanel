d61afae96ab2ae9bb8791a2f788c48fab2b85659
terrydash <53047801@qq.com>
2020-02-28 11:46:32 +0800

    可以启动了！

--- configs/default/db.py
+++ configs/default/db.py
@@ -5,9 +5,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django_prometheus.db.backends.mysql",
         "NAME": "sspanel",
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", "yourpass"),
-        "HOST": os.getenv("MYSQL_HOST", "127.0.0.1"),
-        "USER": os.getenv("MYSQL_USER", "root"),
+        "PASSWORD": "xgxxgx",
+        "HOST": "127.0.0.1",
+        "USER": 'terrydash',
         "OPTIONS": {
             "autocommit": True,
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
